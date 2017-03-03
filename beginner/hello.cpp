0c54402718f619f4dde01d89da736b1bc44724c1
fulkast <fulkast@gmail.com>
2017-03-03 03:49:22 +0100

    add input example

--- beginner/hello.cpp
+++ beginner/hello.cpp
@@ -2,9 +2,22 @@
 // Description: a program that prints the immortal saying, "hellow world"
 
 #include <iostream>
+// the following include is needed for setw() in some c++ implementations
+#include <iomanip>
 using namespace std;
 
 int main() {
-  cout << "Herrow world!" << endl;
+  // set up cout for left-aligned, default would have been right
+  cout << setiosflags(ios::left);
+  // the first for loop will handle the rows
+  for (int i = 0; i < 6; i++) {
+    // the second for loop handles the columsn
+    for (int j = 0; j < 4; j++)
+      // setw(int) sets the column width
+      cout << setw(17) << "Herrow world!";
+    // the next line is a part of the first for loop
+    // it handles the end of line to the next row
+    cout << endl;
+  }
   return 0;
 }
