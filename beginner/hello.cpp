0609eb124743b12777c714abd1571c133cb1407f
fulkast <fulkast@gmail.com>
2017-03-03 03:25:32 +0100

    print hellow world

---
+++ beginner/hello.cpp
@@ -0,0 +1,10 @@
+// hello.cpp: Lanke Fu
+// Description: a program that prints the immortal saying, "hellow world"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Herrow world!" << endl;
+  return 0;
+}
